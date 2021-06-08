4da49d505fc7882d4dff3ab52587318c0f41bfe7
Daniel Bae <danibae91@icloud.com>
2021-06-07 22:52:49 -0700

    change postgresql

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ if os.environ.get('ENV') == 'production':
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://auth_demo'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://auth_demo').replace("://", "ql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 # Heroku Deployment (secretkey)

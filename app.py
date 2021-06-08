ed1bec09d99036bc944c2eb322249f919490db45
Daniel Bae <danibae91@icloud.com>
2021-06-07 22:44:13 -0700

    revise postgresql

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://auth_demo"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://auth_demo').replace("://", "ql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 # Heroku Deployment (secretkey)
